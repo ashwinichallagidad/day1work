8e6dccb96a50f77b6370fdb9e9e2a8ad03b54bb5
ashwinichallagidad <ashwinichallagidad2@gmail.com>
2026-02-26 16:49:56 +0100

    Delete Java directory

--- Java/Remainder.java
+++
@@ -1,9 +0,0 @@
-class Remainder{
-    public static void main(String[]args)
-    {
-        int a=10;
-        int b=2;
-        int remainder=a%b;
-        System.out.println("Remainder:"+remainder);
-    }
-    }
--- Java/Return.java
+++
@@ -1,9 +0,0 @@
-class Return{
-    static String printing(){
-        return ("Hello World");
-    }
-    public static void main(String[] args) {
-        String result=printing();
-        System.out.println(result);
-    }
-}
